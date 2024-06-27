8e4cfb3a9075401575844b97597ef7f2783db7e1
TPLulu <thibault.piotrowski@gmail.com>
2024-06-27 23:46:22 +0200

    test

--- calc/operations.py
+++ calc/operations.py
@@ -1,13 +1,13 @@
-
+# this function is for adding
 def add(a, b):
     return a + b
-
+# this function is for subtract
 def subtract(a, b):
     return a - b
-
+# this function is for multiply
 def multiply(a, b):
     return a * b
-
+# this function is for divide
 def divide(a, b):
     if b == 0:
         raise ZeroDivisionError("Cannot divide by zero")
